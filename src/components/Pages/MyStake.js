73b4cfd84e14d40f11acfe7ed54c0763f4b82d15
coinproof <106302838+kesaviwebsolutions@users.noreply.github.com>
2022-08-17 00:25:21 +0530

    secure-staking-app-updated

--- src/components/Pages/MyStake.js
+++ src/components/Pages/MyStake.js
@@ -3,7 +3,41 @@ import React from 'react'
 export default function MyStake() {
   return (
     <div>
-      my-stake
+      <div className="container">
+        <div className="srpay">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="card card-area">
+                <div className="card-body">
+                  <div className="srpay-content">
+                    <h6 className="srpay-text">Total Staked</h6>
+                    <p className="srpay-count">0 SRPAY</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="card card-area2">
+                <div className="card-body content">
+                  <div className="srpay-content">
+                    <h6 className="srpay-text">Total Earning</h6>
+                    <p className="srpay-count">0 SRPAY</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mystake-content">
+            <div className="mystake">
+              <p>Serial</p>
+              <p>Staking Date</p>
+              <p>Token Amount</p>
+              <p>Staking End</p>
+              <p>Action</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/StakedAmount.js
+++ src/components/StakedAmount.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import logo from "../images/logo.ico";
+import React, { useState } from 'react'
+import logo from '../images/logo.ico'
 
 export default function StakedAmount() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
   return (
     <div>
       <div className="container">
@@ -23,7 +23,7 @@ export default function StakedAmount() {
                   Amount must be greater than zero
                 </p>
               ) : (
-                ""
+                ''
               )}
             </div>
           </div>
@@ -36,10 +36,35 @@ export default function StakedAmount() {
             <div className="stake">
               <h3 className="stakingSummary">Staking Summary</h3>
             </div>
-            
+          </div>
+          <div className="pairs">
+            <div className="single-pairs d-flex  justify-content-around">
+              <h3 className='summary'>Duration :</h3>
+              <p className="values">30 Days</p>
+            </div>
+            <div className="single-pairs d-flex justify-content-around">
+              <h3 className='summary'>APY: </h3>
+              <p className="values">30%</p>
+            </div>
+            <div className="single-pairs d-flex justify-content-around">
+              <h3 className='summary'>Stake Amount: </h3>
+              <p className="values">0 SRPAY</p>
+            </div>
+            <div className="single-pairs d-flex justify-content-around">
+              <h3 className='summary'>Estimated Return: </h3>
+              <p className="values">0 SRPAY</p>
+            </div>
+            <div className="single-pairs d-flex justify-content-around">
+              <h3 className='summary'>Start Date: </h3>
+              <p className="values">Tue, 16 Aug 2022 17:57:34 GMT</p>
+            </div>
+            <div className="single-pairs d-flex justify-content-around">
+              <h3 className='summary'>End Date :</h3>
+              <p className="values">Thu, 15 Sep 2022 17:57:34 GMT</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
