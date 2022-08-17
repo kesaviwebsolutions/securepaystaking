3dbe081da18109999bb5d7529fb8f60b22c7bc28
mayank6253 <89696473+mynkbhati22@users.noreply.github.com>
2022-08-17 09:45:56 +0530

    update

--- src/components/StakingSubmission.js
+++ src/components/StakingSubmission.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { ImCheckboxChecked } from "react-icons/im";
 import StakedAmount from "./StakedAmount";
 
 export default function StakingSubmission() {
+  const [active, setActive] = useState(false);
+  const [unactive, setUnActive] = useState(false);
+  const [active2, setActive2] = useState(false);
+  const [unactive2, setUnActive2] = useState(false);
   return (
     <div>
       <div className="container">
@@ -14,12 +18,14 @@ export default function StakingSubmission() {
         <div className="stakingsubmission-cards">
           <div className="row">
             <div className="col-lg-3 col-xl-3 col-lg-6 col-md-6 col-sm-12">
-              <div className="card card-section active">
-                <ImCheckboxChecked
-                  size={32}
-                  color="#3cc05d"
-                  className="checkIcon"
-                />
+              <div
+                className={`card card-section ${!unactive ? "active" : ""}`}
+                onClick={() => {
+                  setUnActive(false);
+                  setActive(false);
+                }}
+              >
+                <ImCheckboxChecked size={32} color="#3cc05d" id="checkIcon" />
                 <div className="card-body ss-text">
                   <h5 className="card-title styling">90 Days</h5>
                   <h6 className="card-subtitle mb-2 subtitleStyling">
@@ -29,7 +35,13 @@ export default function StakingSubmission() {
               </div>
             </div>
             <div className="col-lg-3 col-xl-3 col-lg-6 col-md-6 col-sm-12">
-              <div className="card card-section">
+              <div
+                className={`card card-section ${active ? "active" : ""}`}
+                onClick={() => {
+                  setActive(true);
+                  setUnActive(true);
+                }}
+              >
                 <div className="card-body ss-text">
                   <h5 className="card-title styling">180 Days</h5>
                   <h6 className="card-subtitle mb-2 subtitleStyling">
@@ -39,7 +51,7 @@ export default function StakingSubmission() {
               </div>
             </div>
             <div className="col-lg-3 col-xl-3 col-lg-6 col-md-6 col-sm-12">
-              <div className="card card-section">
+              <div className={`card card-section ${active2 ? "active" : ""}`} onClick={()=>setActive2(true)}>
                 <div className="card-body ss-text">
                   <h5 className="card-title styling">365 Days</h5>
                   <h6 className="card-subtitle mb-2 subtitleStyling">
